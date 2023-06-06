41640bb746c45e05edde058ef3071fcd6387c5fd
Daniel Mateu <danielmateu86@gmail.com>
2023-06-06 13:24:43 +0200

    gitignore added

--- ejercicios/areaCirculo/areaCirculo.php
+++ ejercicios/areaCirculo/areaCirculo.php
@@ -7,6 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Área Circulo</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <!-- <link rel="stylesheet" href="../../styles.css"> -->
 </head>
 
 <body class="container">
@@ -14,6 +15,9 @@
 
     <p>Escribe un programa que calcule el diámetro, perímetro y área de
         un círculo a partir de su radio.</p>
+
+    <p>La fórmula del diámetro es 2*r, la del perímetro 2*PI*r y la del
+        área PI*r2. </p>
 
     <form method="get" class="d-flex flex-column col-5">
         <label for="radio">Radio</label>
