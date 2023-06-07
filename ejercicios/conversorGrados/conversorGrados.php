42e93dd573738615ed6590b94af6fcea998894cf
Daniel Mateu <danielmateu86@gmail.com>
2023-06-07 09:05:31 +0200

    Ejercicios basicos con algo de JS

--- ejercicios/conversorGrados/conversorGrados.php
+++ ejercicios/conversorGrados/conversorGrados.php
@@ -7,11 +7,29 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Conversor Grados</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-
+    <link rel="stylesheet" href="../../styles.css">
 </head>
 
 <body class="container">
-    <h1>Conversor de Fahrenheit a Celisus</h1>
+
+    <header class="d-flex flex-col justify-content-center  p-2 mb-4 border-bottom position-sticky top-0 start-0  col-12" id="menu">
+        <h1 class=" ">Conversor de Fahrenheit a Celisus</h1>
+        <nav class="navbar navbar-expand-lg navbar-dark col-6 d-flex justify-content-around">
+            <!-- <a href="#if">if</a>
+            <a href="#ifelse">if-else</a>
+            <a href="#switch">switch</a>
+            <a href="#operadorTernario">operador ternario</a>
+            <a href="#operadorNull">operador ??</a> -->
+        </nav>
+        <!-- Boton para cambio de modo -->
+
+        <div class="form-check form-switch position-absolute top-0 end-0 me-1 mt-1">
+            <input class="form-check-input" type="checkbox" id="flexSwitchCheckDefault">
+            <!-- <label class="form-check-label" for="flexSwitchCheckDefault">Modo</label> -->
+        </div>
+
+    </header>
+
 
     <p>Escribe un programa que convierta de grados Fahrenheit a grados
         Celsius.</p>
@@ -28,7 +46,9 @@
 
     <form action="" method="post" class="d-flex flex-column col-5">
         <label for="fahrenheit">Fahrenheit</label>
-        <input type="number" name="fahrenheit" id="fahrenheit" placeholder="Introduce los grados Fahrenheit">
+        <input type="number" name="fahrenheit" id="fahrenheit" placeholder="Introduce los grados Fahrenheit"
+        
+        >
 
         <input type="submit" value="calcular" class="btn btn-primary mt-2" name="calcular">
 
@@ -48,7 +68,12 @@
     }
     ?>
 
+    <footer class="p-2 mt-5 border-top text-center">
+        &copy; <?= date('Y') ?> - Desarrollo Web en Entorno Servidor
 
+    </footer>
+
+    <script src="./index.js"></script>
 </body>
 
 </html>
