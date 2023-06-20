27d1c38db66edbdd77bf0ea2c0ff30e49a1f5d4c
Daniel Mateu <danielmateu86@gmail.com>
2023-06-20 10:08:29 +0200

    galeria usando scandir

---
+++ ejemplos/07-galeriaDinamicaScandir/index.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Galeria dinámica scandir()</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="../../styles.css">
+
+</head>
+
+<body class="container p-4 position-relative">
+
+    <header class="header d-flex flex-column bg-dark  p-4 mb-4 position-sticky top-0 start-0  col-12 rounded opacity-4" id="menu">
+        <h1 class="col-6 text-white">Galeria dinámica scandir()</h1>
+
+        <div class="form-check form-switch position-absolute top-0 end-0 me-1 mt-1">
+            <input class="form-check-input" type="checkbox" id="flexSwitchCheckDefault">
+        </div>
+
+    </header>
+
+    <main class="p-2">
+
+        <section class="mb-5" id="ejemplo">
+            <form method="post" enctype="multipart/form-data" action="upload.php" class="form mb-4">
+                <div>
+                    <label for="file-with-preview">Sube una imagen</label>
+                    <input type="hidden" name="MAX_FILE_SIZE" value="1250000000" class="form-control">
+                    <input type="file" name="fichero" id="file-with-preview" class="form-control" accept=".jpg, .jpeg, .gif, .png">
+                </div>
+
+                <br>
+
+                <input type="submit" value="Enviar" class="btn btn-primary">
+            </form>
+
+            <!-- <figure>
+                <img src="./imagenes/default-image.png" alt="default image" id="preview-image" class="img-fluid rounded">
+            </figure> -->
+
+            <!-- Galeria -->
+
+            <?php
+            class UploadException extends Exception
+            {
+            }
+
+            $archivos = scandir('./imagenes');
+
+            // Eliminamos los dos primeros elementos del array
+            unset($archivos[0]);
+            unset($archivos[1]);
+
+            // Ordenamos el array
+            sort($archivos);
+
+            ?>
+
+            <div class="galeria rounded ">
+                <?php
+                // Listamos los archivos
+                foreach ($archivos as $archivo) {
+                    echo "<figure class='figure'>";
+                    echo "<img src='./imagenes/$archivo' alt='$archivo' class='img-fluid rounded'>";
+                    echo "<figcaption class='figure-caption'>$archivo</figcaption>";
+                    echo "</figure>";
+                }
+                ?>
+            </div>
+
+
+        </section>
+
+
+
+    </main>
+
+    <button class="btn btn-secondary btn-lg d-block mx-auto mt-5" id="btn-scroll">
+        Volver al principio
+    </button>
+
+    <footer class="p-2 mt-5 border-top text-center">
+        &copy; <?= date('Y') ?> - Desarrollo Web en Entorno Servidor
+    </footer>
+
+    <script src="../../index.js"></script>
+</body>
+
+</html>
---
+++ ejemplos/07-galeriaDinamicaScandir/upload.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Galeria dinámica</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="../../styles.css">
+
+</head>
+
+<body class="container p-4 position-relative">
+
+    <header class="header d-flex flex-column bg-dark  p-4 mb-4 position-sticky top-0 start-0  col-12 rounded opacity-4" id="menu">
+        <h1 class="col-6 text-white">Galeria dinámica</h1>
+
+        <div class="form-check form-switch position-absolute top-0 end-0 me-1 mt-1">
+            <input class="form-check-input" type="checkbox" id="flexSwitchCheckDefault">
+
+        </div>
+
+    </header>
+
+    <main class="p-1">
+
+        <h2>Datos del fichero recibido en el servidor</h2>
+
+        <section class="mb-5" id="ejemplo">
+
+            <pre>
+                <?php
+                class UploadException extends Exception
+                {
+                }
+
+                require_once './libraries/Upload.php';
+
+                // Sube el fichero, hace las comprobaciones y retorna la ruta del fichero subido
+                $ruta = Upload::save(
+                    'fichero',
+                    'imagenes',
+                    true,
+                    1048576,
+                    'image/*',
+                    'img_',
+                    true
+                );
+
+
+
+                // Muestra el mensaje de exito
+                echo '
+                    <div class = "d-flex flex-column justify-content-center align-items-center bg-success p-5 rounded">
+                        <h3>El fichero se ha subido correctamente!</h3>
+                        <h3>😊</h3>
+                        <img src="' . $ruta . '" alt="Imagen subida" class="img-fluid">
+                    </div>
+
+                    ';
+                ?>
+            </pre>
+
+        </section>
+
+
+
+    </main>
+
+    <button class="btn btn-secondary btn-lg d-block mx-auto mt-5" id="btn-scroll">
+        Volver al principio
+    </button>
+
+    <footer class="p-2 mt-5 border-top text-center">
+        &copy; <?= date('Y') ?> - Desarrollo Web en Entorno Servidor
+    </footer>
+
+    <script src="../../index.js"></script>
+</body>
+
+</html>
