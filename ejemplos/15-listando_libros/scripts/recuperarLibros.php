e180816e7635792902c3d5231aa724ad67d6a823
Daniel Mateu <danielmateu86@gmail.com>
2023-06-27 11:38:30 +0200

    más ejemplos

--- ejemplos/15-listando_libros/scripts/recuperarLibros.php
+++ ejemplos/15-listando_libros/scripts/recuperarLibros.php
@@ -1,6 +1,8 @@
 <?php
 // lanza la consulta seleccionando todos los campos de la tabla libros
-$resultado = $conexion->query("SELECT * FROM libros");
+// $resultado = $conexion->query("SELECT * FROM libros");
+//  Lanza consulta que selecciona todos los campos de la tabla libros y los ordena por id de forma ascendente
+$resultado = $conexion->query("SELECT * FROM libros ORDER BY id ASC LIMIt 10");
 
 // Preparamos una lista de libros
 $libros = [];
